947120935b08e0fe78b014d105a8454fb8b1b5a1
Rodrigo Fernandes <rodrigoserrafernandes98@gmail.com>
2021-04-16 23:41:04 +0100

    First commit

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[400],
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text("I'm a title"),
+        ),
+        body: Image(
+
+        ),
+      )
+  )
+  );
+}
