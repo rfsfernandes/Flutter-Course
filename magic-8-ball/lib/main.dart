b4b538685e60ce3a65f6a80848fce0857ba76dcc
Rodrigo Fernandes <rodrigoserrafernandes98@gmail.com>
2021-04-17 22:56:11 +0100

    Last commit

--- magic-8-ball/lib/main.dart
+++ magic-8-ball/lib/main.dart
@@ -1,7 +1,54 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  updateBall() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: InkWell(
+        child: Image.asset("images/ball$ballNumber.png"),
+        onTap: () => updateBall(),
+      ),
+    );
+  }
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.shade500,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        centerTitle: true,
+        title: Text("Ask me anything"),
+      ),
+      body: Container(
+        child: Center(
+          child: Ball(),
+        ),
+      ),
+    );
+  }
+}
