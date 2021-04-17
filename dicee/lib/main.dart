595ab1e6ce9a918c6deae9d6b547e345c7d03729
Rodrigo Fernandes <rodrigoserrafernandes98@gmail.com>
2021-04-17 22:34:09 +0100

    Final commit

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,50 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int random1 = 1;
+  int random2 = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    void buttonsClicked() {
+      setState(() {
+        random1 = Random().nextInt(6) + 1;
+        random2 = Random().nextInt(6) + 1;
+      });
+    }
+
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: InkWell(
+                onTap: () => buttonsClicked(),
+                child: Image.asset("images/dice$random1.png"),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: InkWell(
+                onTap: () => buttonsClicked(),
+                child: Image.asset("images/dice$random2.png"),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
